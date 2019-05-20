abce4590110464864b3b948be43d4a810e6bf091
ivrybachenko <ivrybachenko@gmail.com>
2019-05-20 10:47:03 +0700

    UserService injection to DAOSwitchCommand
    I have nullPointerException when accessing property user. It seems that wiring wasn't successful.

--- user/user-service/src/main/java/internship/services/userService/DAOSwitchCommand.java
+++ user/user-service/src/main/java/internship/services/userService/DAOSwitchCommand.java
@@ -1,13 +1,20 @@
 package internship.services.userService;
 
+import internship.dao.userDAO.UserDatabaseDAO;
 import org.apache.karaf.shell.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
 
 @Command(scope = "user", name = "dao-db", description = "Switch to DB DAO.")
 public class DAOSwitchCommand extends OsgiCommandSupport {
+    public void setUser(UserServiceImpl user) {
+        this.user = user;
+    }
+
+    private UserServiceImpl user;
     @Override
     protected Object doExecute() throws Exception {
         System.out.println("Switching to DB.");
+        user.setDao(new UserDatabaseDAO());
         return null;
     }
 }
