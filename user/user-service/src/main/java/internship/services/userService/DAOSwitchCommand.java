df5c7e0a15d3c1077475df0b03ff112a3cbee169
ivrybachenko <ivrybachenko@gmail.com>
2019-05-20 12:00:28 +0700

    Added command attribute.

--- user/user-service/src/main/java/internship/services/userService/DAOSwitchCommand.java
+++ user/user-service/src/main/java/internship/services/userService/DAOSwitchCommand.java
@@ -1,11 +1,16 @@
 package internship.services.userService;
 
 import internship.dao.userDAO.UserDatabaseDAO;
+import org.apache.karaf.shell.commands.Argument;
 import org.apache.karaf.shell.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
 
-@Command(scope = "user", name = "dao-db", description = "Switch to DB DAO.")
+@Command(scope = "user", name = "dao", description = "Switch to DB DAO.")
 public class DAOSwitchCommand extends OsgiCommandSupport {
+
+    @Argument(index = 0, name = "dao", description = "The new dao to be set. Could be either 'db' or 'hm'.", required = true, multiValued = false)
+    String dao;
+
     public void setUser(UserServiceImpl user) {
         this.user = user;
     }
@@ -13,8 +18,13 @@ public class DAOSwitchCommand extends OsgiCommandSupport {
     private UserServiceImpl user;
     @Override
     protected Object doExecute() throws Exception {
-        System.out.println("Switching to DB.");
-        user.setDao(new UserDatabaseDAO());
+        if (dao.equals("db")) {
+            System.out.println("Switching to DB.");
+            user.setDao(new UserDatabaseDAO());
+        }
+        else{
+            System.out.println("Wrong dao. Could be either 'db' or 'hm'.");
+        }
         return null;
     }
 }
