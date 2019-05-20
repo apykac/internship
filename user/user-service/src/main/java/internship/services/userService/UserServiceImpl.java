34f8ebcb089045158b7b0b9b86aa960bee76c41b
ivrybachenko <ivrybachenko@gmail.com>
2019-05-20 14:49:39 +0700

    HttpGet invocation
    TODO: unmarshal response-string to object

--- user/user-service/src/main/java/internship/services/userService/UserServiceImpl.java
+++ user/user-service/src/main/java/internship/services/userService/UserServiceImpl.java
@@ -6,21 +6,44 @@ import internship.dao.userDAO.UserHashMapDAO;
 import internship.models.userModel.User;
 import internship.models.userModel.dto.UserDTO;
 import internship.validators.userValidator.UserValidator;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 public class UserServiceImpl implements UserService {
 
     private UserDAO userDao = UserHashMapDAO.getInstance();
-    private UserDAO userDbDao = new UserDatabaseDAO();
 
     @GET
     @Path("/users/{id}/")
     @Produces("application/xml")
     public User getUser(@PathParam("id") Long id) {
         System.out.println("get user " + id);
-        return userDbDao.findUserById(id);
+        return userDao.findUserById(id);
+    }
+
+    @GET
+    @Path("/users2/{id}")
+    public String getAddress(){
+        HttpClient client = new HttpClient();
+        HttpMethod method = new GetMethod("http://localhost:8181/cxf/userservice/users/1");
+        try {
+            client.executeMethod(method);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String s = null;
+        try {
+            s = method.getResponseBodyAsString();
+        } catch (IOException e) {
+            s = "error";
+            e.printStackTrace();
+        }
+        return  s;
     }
 
     @PUT
@@ -28,7 +51,7 @@ public class UserServiceImpl implements UserService {
     public Response updateUser(@PathParam("id") Long id, UserDTO user) {
         UserValidator userValidator = new UserValidator(user);
         if (userValidator.isValid()) {
-            User updatedUser = userDbDao.updateUser(id, user);
+            User updatedUser = userDao.updateUser(id, user);
             return Response.ok().type("application/xml").entity(updatedUser).build();
         } else {
             return Response.status(Response.Status.BAD_REQUEST).entity(userValidator.getErrorMessage()).build();
@@ -40,7 +63,7 @@ public class UserServiceImpl implements UserService {
     public Response addUser(UserDTO user) {
         UserValidator userValidator = new UserValidator(user);
         if (userValidator.isValid()) {
-            User newUser = userDbDao.createUser(user);
+            User newUser = userDao.createUser(user);
             return Response.ok().type("application/xml").entity(newUser).build();
         } else {
             return Response.status(Response.Status.BAD_REQUEST).entity(userValidator.getErrorMessage()).build();
@@ -50,7 +73,7 @@ public class UserServiceImpl implements UserService {
     @DELETE
     @Path("/users/{id}/")
     public Response deleteUser(@PathParam("id") Long id) {
-        userDbDao.removeUser(id);
+        userDao.removeUser(id);
         return Response.ok().build();
     }
 }
