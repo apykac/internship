45ba41b5fb7563b171ce1d652119b36c93bebcfe
ivrybachenko <ivrybachenko@gmail.com>
2019-06-03 12:43:15 +0700

    Just some TODOs added

--- address/address-sort/src/main/java/internship/services/addressSort/IAddressSort.java
+++ address/address-sort/src/main/java/internship/services/addressSort/IAddressSort.java
@@ -5,5 +5,12 @@ import internship.models.addressModel.Address;
 import java.util.List;
 
 public interface IAddressSort {
+
+    // TODO: Написать по какому принципу сортируются адреса.
+    /**
+     * Отсортировать список адресов.
+     * @param address Адреса для сортировки
+     * @return Возвращает отсортированный список адресов
+     */
     List<Address> sort(List<Address> address);
 }
